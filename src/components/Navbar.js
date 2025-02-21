d560a232ae207b9c4f78c9d84aa7e5d5f36b6319
TusharGhosh56 <tusharghosh408@gmail.com>
2025-02-21 10:08:11 +0530

    added navigation to home on logo click

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,26 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { auth } from '../firebase/config'; // Ensure Firebase is properly imported
+import { auth } from '../firebase/config'; 
 import { onAuthStateChanged } from 'firebase/auth';
 import '../css/Navbar.css';
 
+
 function Navbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  const navigate = useNavigate();
   useEffect(() => {
-    // Listen for auth state changes
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setIsLoggedIn(!!user);
     });
 
-    return () => unsubscribe(); // Cleanup on unmount
+    return () => unsubscribe(); 
   }, []);
 
   return (
     <nav className="navbar">
-      <div className="nav-brand">FitClub-Fitness with Community</div>
+      <div className="nav-brand">
+        <button className='gym-name' onClick={()=>navigate('/')}>FitClub-Fitness with Community</button>
+        </div>
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/programs">Programs</Link></li>
